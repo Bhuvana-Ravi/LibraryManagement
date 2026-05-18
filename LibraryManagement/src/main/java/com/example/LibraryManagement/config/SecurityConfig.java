33f96e758b416e49c51e9616b294a198b7ede775
Bhuvana-Ravi <bhuvana11143@gmail.com>
2026-05-18 11:29:48 +0530

    Fixed hello endpoint

--- LibraryManagement/src/main/java/com/example/LibraryManagement/config/SecurityConfig.java
+++ LibraryManagement/src/main/java/com/example/LibraryManagement/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
                     .csrf(csrf->csrf.disable())
                     .authorizeHttpRequests(auth-> auth
                             .requestMatchers(
-                                    "/hello",
+                                    "/books/hello",
                                     "/books/token",
                                     "/books/validate"
                             ).permitAll()
